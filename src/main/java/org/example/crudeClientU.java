9c95b79ede4fc92a2dc01c750b1b890c8adc8233
mathias124 <mfs2001@hotmail.dk>
2023-03-10 15:46:15 +0100

    First Push Client

--- src/main/java/org/example/crudeClientU.java
+++ src/main/java/org/example/crudeClientU.java
@@ -6,67 +6,98 @@ import java.util.*;
 
 public class crudeClientU {
 
-    public static void main(String argv[])
-    {
+    public static void main(String argv[]) {
         Scanner in = new Scanner(System.in);
+        String M = "helo martin" +"\r\n" ;
 
-        try{
-            Socket socketClient= new Socket("datacomm.bhsi.xyz",2526);
+        try {
+            Socket socketClient = new Socket("datacomm.bhsi.xyz", 2526);
 
-            System.out.println("Client: "+"Connection Established");
+            System.out.println("Client: " + "Connection Established");
 
             BufferedReader reader =
                     new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
 
-            BufferedWriter writer=
+            BufferedWriter writer =
                     new BufferedWriter(new OutputStreamWriter(socketClient.getOutputStream()));
 
-            String s,serverMsg;
+            String s, serverMsg;
 
 
-            while((serverMsg = reader.readLine()) != null){
+            while ((serverMsg = reader.readLine()) != null) {
                 System.out.println(serverMsg);
+               if ((serverMsg.startsWith("220"))) {
 
-                if(serverMsg.startsWith("354")){
-                    s = in.nextLine();
-                    writer.write(s + "\r\n");
+                    writer.write(M);
+
                     writer.flush();
 
-                    s = in.nextLine();
-                    writer.write(s + "\r\n");
-                    writer.flush();
-
-                    s = in.nextLine();
-                    writer.write(s + "\r\n");
-                    writer.flush();
-
-                    s = in.nextLine();
-                    writer.write(s + "\r\n");
-                    writer.flush();
-
-                    s = in.nextLine();
-                    writer.write(s + "\r\n");
-                    writer.flush();
-
-                    s = in.nextLine();
-                    writer.write(s + "\r\n");
-                    writer.flush();
-
-
+                    //System.out.println(s + "k");
                 }
-                else {
+                if ((serverMsg.equals("250 Nice to meet you"))) {
+                    System.out.println("Please enter your Email adress:");
+                    s = in.nextLine();
+                    String mailCommand ="mail from:";
+                    writer.write(mailCommand + s );
+
+                    //writer.flush();
+                }
+                if ((serverMsg.equals("250 New message started"))) {
+                    System.out.println("Please enter a desired email receptient:");
+                    String B = in.nextLine();
+                    String mailCommand ="rcpt to :";
+                    writer.write( mailCommand + "<" + B + ">");
+                    //System.out.println(B);
+                    //writer.flush();
+                }
+
+
+                if (serverMsg.startsWith("354")) {
+                    s = in.nextLine();
+                    writer.write(s + "\r\n");
+                    writer.flush();
+
+                    s = in.nextLine();
+                    writer.write(s + "\r\n");
+                    writer.flush();
+
+                    s = in.nextLine();
+                    writer.write(s + "\r\n");
+                    writer.flush();
+
+                    s = in.nextLine();
+                    writer.write(s + "\r\n");
+                    writer.flush();
+
+                    s = in.nextLine();
+                    writer.write(s + "\r\n");
+                    writer.flush();
+
+                    s = in.nextLine();
+                    writer.write(s + "\r\n");
+                    writer.flush();
+
+
+                } else {
                     s = in.nextLine();
                     writer.write(s + "\r\n");
                     writer.flush();
                 }
+
 
 
             }
 
-        } catch(Exception e) {
-            e.printStackTrace();
+            } catch(UnknownHostException ex){
+                throw new RuntimeException(ex);
+            } catch(IOException ex){
+
+
+            } catch(Exception e){
+                e.printStackTrace();
+            }
         }
     }
-}
+
 
 
