200bd9199a8480d6def0b135b1a6767408952516
kunkunshark <s226672@student.dtu.dk>
2023-03-11 23:37:32 +0100

    seperate mail content to subject and body

--- src/main/java/org/example/crudeClientU.java
+++ src/main/java/org/example/crudeClientU.java
@@ -12,6 +12,7 @@ public class crudeClientU {
         String senderSyntax    = "mail from: ";
         String recipientSyntax = "rcpt to: ";
         String messageSyntax   = "data";
+        String quitSyntax      = "quit";
 
         try {
 
@@ -29,49 +30,66 @@ public class crudeClientU {
 
             while ((serverMsg = reader.readLine()) != null) {
                 System.out.println(serverMsg);
-                if ((serverMsg.startsWith("220 Welcome"))) {
+                if (serverMsg.startsWith("220 Welcome")) {
                     System.out.println("[SERVER RESPONSE] Connection Established\n");
                     writer.write(startSyntax + "\r\n");
                     writer.flush();
 
-                } else if ((serverMsg.startsWith("250 comit.dev"))) {
+                } else if (serverMsg.startsWith("250 comit.dev")) {
                     System.out.println("[SERVER RESPONSE] Successful Login");
                     System.out.println("\nPlease enter the sender email address");
                     String senderInput = in.nextLine();
                     writer.write(senderSyntax + senderInput + "\r\n");
                     writer.flush();
 
-                } else if ((serverMsg.startsWith("250 2.1.0"))) {
+                } else if (serverMsg.startsWith("250 2.1.0")) {
                     System.out.println("\nPlease enter a recipient email address");
                     String recipientInput = in.nextLine();
                     writer.write(recipientSyntax + recipientInput + "\r\n");
                     writer.flush();
 
-                } else if ((serverMsg.startsWith("250 2.1.5"))) {
+                } else if (serverMsg.startsWith("250 2.1.5")) {
                     writer.write(messageSyntax + "\r\n");
                     writer.flush();
                     System.out.print("\n");
 
                 } else if (serverMsg.startsWith("354")) {
-                    System.out.println("[SERVER RESPONSE] Email Addresses Confirmed");
-                    boolean run = true;
+                    System.out.println("[SERVER RESPONSE] Sender and Recipient Email Addresses Confirmed");
+                    System.out.println("\n[SERVER RESPONSE] Proceed with the Email Content");
 
-                    while (run) {
-                        if (in.hasNext(".")) {
-                            run = false;
-                            writer.flush();
+                    // taking input for mail subject
+                    System.out.println("Please enter the mail subject");
+                    Scanner subject = new Scanner(System.in);
+                    String mailSubject = subject.nextLine();
+
+                    // taking input for mail content
+                    System.out.println("\nPlease enter the mail content");
+                    System.out.println("End the message with a \".\"");
+                    Scanner body = new Scanner(System.in);
+                    String mailContent = "";
+
+                    boolean inputMessage = true;
+                    while(inputMessage) {
+                        if (body.hasNext(".")) {
+                            inputMessage = false;
                         }
-
-                        if (in.hasNext("\r\n")) {
-                            s = "";
-                            writer.write(s + "\r\n");
-                            writer.flush();
-                        }
-
-                        s = in.nextLine();
-                        writer.write(s + "\r\n");
-                        writer.flush();
+                        mailContent = mailContent + "\n" + body.nextLine();
                     }
+                    String mail = "Subject: " + mailSubject + "\n" + mailContent;
+
+                    writer.write(mail + "\r\n");
+                    writer.flush();
+                    System.out.print("\n");
+
+                } else if (serverMsg.startsWith("250 2.0.0 Ok: queued")) {
+                    writer.write(quitSyntax + "\r\n");
+                    writer.flush();
+                    System.out.println("[SERVER RESPONSE] Success! Email is being sent out to Recipient");
+                    System.out.print("\n");
+
+                } else if (serverMsg.startsWith("221 2.0.0")) {
+                    System.out.println("[CONNECTION] Connection Terminated");
+
                 } else {
                     s = in.nextLine();
                     writer.write(s + "\r\n");
